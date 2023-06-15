8ec52f2581b7039efbde06c97e3867690aba420d
ManoaRRR <hei.manoa.2@gmail.com>
2023-06-15 10:00:12 +0300

    feat:add task manager

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,90 @@
+import { useState, useEffect } from "react";
 import Head from "next/head";
 import { useRouter } from "next/router";
 
-/**
-  Calculates the time difference between the server time and client time.
-  @param {Date} serverTime - The server time.
-  @param {Date} clientTime - The client time.
-  @returns {string} The time difference in the format "{days} days, {hours} hours, {minutes} minutes, {seconds} seconds".
-*/
-const calculateTimeDifference = (server: Date, client: Date) => {};
+interface Task {
+  id: string;
+  title: string;
+  completed: boolean;
+}
 
+interface TasksState {
+  tasks: Task[];
+  searchQuery: string;
+}
+
+type TaskAction =
+  | { type: "ADD_TASK"; task: Task }
+  | { type: "UPDATE_TASK"; id: string; completed: boolean }
+  | { type: "DELETE_TASK"; id: string }
+  | { type: "SET_SEARCH_QUERY"; query: string };
+
+const useTasks = () => {
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [searchQuery, setSearchQuery] = useState("");
+
+  useEffect(() => {
+    // Load tasks from Local Storage on component mount
+    const storedTasks = localStorage.getItem("tasks");
+    if (storedTasks) {
+      setTasks(JSON.parse(storedTasks));
+    }
+  }, []);
+
+  useEffect(() => {
+    // Save tasks to Local Storage when tasks state changes
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
+  const dispatch = (action: TaskAction) => {
+    switch (action.type) {
+      case "ADD_TASK":
+        setTasks([...tasks, action.task]);
+        break;
+      case "UPDATE_TASK":
+        setTasks((prevTasks) =>
+          prevTasks.map((task) =>
+            task.id === action.id ? { ...task, completed: action.completed } : task
+          )
+        );
+        break;
+      case "DELETE_TASK":
+        setTasks((prevTasks) => prevTasks.filter((task) => task.id !== action.id));
+        break;
+      case "SET_SEARCH_QUERY":
+        setSearchQuery(action.query);
+        break;
+      default:
+        break;
+    }
+  };
+
+  const filteredTasks = tasks.filter((task) =>
+    task.title.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
+  return { tasks: filteredTasks, searchQuery, dispatch };
+};
+
+const calculateTimeDifference = (server: Date, client: Date) => {
+  const diff = Math.abs(server.getTime() - client.getTime()) / 1000;
+  const days = Math.floor(diff / 86400);
+  const hours = Math.floor((diff % 86400) / 3600);
+  const minutes = Math.floor((diff % 3600) / 60);
+  const seconds = Math.floor(diff % 60);
+  return `${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds`;
+};
 
 export default function Home() {
   const router = useRouter();
+  const { tasks, searchQuery, dispatch } = useTasks();
+  const serverTime = new Date(); // Replace with your server time
+  const clientTime = new Date();
+
   const moveToTaskManager = () => {
     router.push("/tasks");
-  }
+  };
+
   return (
     <>
       <Head>
@@ -26,16 +96,11 @@ export default function Home() {
       <main>
         <h1>The easiest exam you will ever find</h1>
         <div>
-          {/* Display here the server time (DD-MM-AAAA HH:mm)*/}
           <p>
-            Server time:{" "}
-            <span className="serverTime">{/* Replace with the value */}</span>
+            Server time: <span className="serverTime">{serverTime.toLocaleString()}</span>
           </p>
-
-          {/* Display here the time difference between the server side and the client side */}
           <p>
-            Time diff:{" "}
-            <span className="serverTime">{/* Replace with the value */}</span>
+            Time diff: <span className="serverTime">{calculateTimeDifference(serverTime, clientTime)}</span>
           </p>
         </div>
 
